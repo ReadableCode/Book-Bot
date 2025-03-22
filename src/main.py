1d68332e0d54c2339ac88a46e154c17c2d2d2ad2
jason <jasonwchristiansen@gmail.com>
2025-03-22 15:56:28 -0500

    remove unused variables

--- src/main.py
+++ src/main.py
@@ -1,25 +1,8 @@
 # %%
 # Imports #
 
-
-import os
-
 from local_database_postgres import query_postgres
 from utils.display_tools import pprint_df, pprint_dict, pprint_ls  # noqa
-
-# %%
-# Variables #
-
-
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-book_data_dir = os.path.join("F:\\", "book-data")
-print(book_data_dir)
-
-verbose = False
-data_dumps_url = "https://openlibrary.org/developers/dumps"
-COMMIT_EVERY_ROW_NUM = 100000
-MAX_ROWS_TO_READ = None  # Set to None to read all rows
-
 
 # %%
 # Sample Functions #
